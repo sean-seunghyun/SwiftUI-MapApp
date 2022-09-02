b5c29f6c66a2bd78c35bada413a93a2931f6b103
sean <flyseankim93@gmail.com>
2022-09-02 12:33:58 +0900

    Initial Commit

---
+++ SwiftUI-MapApp/SwiftUI_MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MapAppApp.swift
+//  SwiftUI-MapApp
+//
+//  Created by sean on 2022/09/02.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
